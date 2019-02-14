cd1e4e52354d3c901f36ea1afa66dae7bf8e07a5
shalvah <diakon.ng@gmail.com>
2019-02-15 00:10:50 +0100

    Implement logic

--- index.js
+++ index.js
@@ -1,9 +1,70 @@
 'use strict';
 
-exports.http = (request, response) => {
-  response.status(200).send('Hello World!');
-};
+async function getTweetsOnThisDay(username, today = new Date()) {
+    const baseDate = today;
 
-exports.event = (event, callback) => {
-  callback();
+    // Generate an array of all the years we're looking for tweets in.
+    // Twitter was founded in 2006, so we only need to go as far as that.
+    const years = Array.from(
+        {length: parseInt(today.getFullYear()) - 2006},
+        (_, i) => 2006 + i
+    );
+
+    const requests = years.map(year => {
+        baseDate.setFullYear(year);
+        return getTweetsMadeInYear(username, baseDate)
+    });
+
+    let results = await Promise.all(requests);
+    return years.reduce((tweets, year, index) => {
+        tweets[year] = results[index];
+        return tweets;
+    }, {});
+}
+
+function getTweetsMadeInYear(username, date) {
+    const url = `https://mobile.twitter.com/search?q=`;
+    const got = require("got");
+    const cheerio = require('cheerio');
+    const searchQuery = getSearchQuery(username, date);
+
+    return got(`${url}${searchQuery}`, {headers: {'user-agent': null}})
+        .then(r => {
+            const $ = cheerio.load(r.body);
+            return $('.tweet-text')
+                .map(function extractTweetUrlFromDomNode(_, node) {
+                    return `http://twitter.com/${username}/status/${node.attribs['data-id']}`;
+                }).get();
+
+        })
+        .then(tweets => Promise.all(tweets.map(getTweetEmbedHtml)));
+}
+
+function getSearchQuery(username, baseDate) {
+    const endDate = new Date(baseDate);
+    endDate.setDate(parseInt(baseDate.getDate()) + 1);
+    const start = baseDate.toISOString().replace(/T.*/, '');
+    const end = endDate.toISOString().replace(/T.*/, '');
+    return `from:${username} since:${start} until:${end}`;
+}
+
+function getTweetEmbedHtml(tweetUrl) {
+    const got = require("got");
+    const qs = require('querystring');
+    const url = 'https://publish.twitter.com/oembed?' + qs.stringify({ url: tweetUrl, theme: 'dark' });
+    return got(url, {headers: {'user-agent': null}})
+        .then(r => qs.unescape(JSON.parse(r.body).html));
+}
+
+exports.getTweetsOnThisDay = (req, res) => {
+    switch (req.method) {
+        case 'POST':
+            let result = getTweetsOnThisDay(req.query.username);
+            return res.status(200).send(JSON.stringify(result));
+        case 'OPTIONS':
+            // CORS preflight? Gotcha.
+
+        default:
+        // I don't fucking care
+    }
 };
